b91b7686a393f6e2e6171759e599203c925e1067
Stefan <stefan_mack@hotmail.com>
2018-05-24 11:42:32 +0200

    now does math

--- Ares.py
+++ Ares.py
@@ -12,7 +12,7 @@ ares_quotes= \
 "Go ask marvin",
 "No",
 "Why?",
-r"¯\_(ツ)_/¯",
+r"¯\\_(ツ)_/¯",
 ]
 
 import praw
@@ -24,11 +24,13 @@ import sys
 reddit = praw.Reddit('Ares')
 subreddit = reddit.subreddit("pythonforengineers")
 
-    
-if not os.path.isfile("responded.txt"):
+fil = "responded.txt"    
+if not os.path.isfile(fil):
     comments_responded = []
+    with open(fil, "w") as f:
+        pass
 else:
-    with open("responded.txt", "r") as f:
+    with open(fil, "r") as f:
         comments_responded = f.read()
         comments_responded = comments_responded.split("\n")
         comments_responded = list(filter(None, comments_responded))
@@ -36,14 +38,37 @@ else:
 for comment in subreddit.stream.comments():
     if comment.id not in comments_responded:
         if re.search("Ares", comment.body, re.IGNORECASE):
-            ares_reply = random.choice(ares_quotes)
-            while True:
-                try:
-                    comment.reply(ares_reply)
-                    comments_responded.append(comment.id)
-                    with open("responded.txt", "w") as f:
-                        f.write(comment.id + "\n")
-                    break
-                except praw.exceptions.APIException:
-                    time.sleep(600)
+            if not comment.body.find('/') == -1:
+                start = comment.body.find('/')
+                if not comment.body.find('\\') == -1:
+                    stop = comment.body.find('\\')
+                    expression = comment.body[(start+1):stop]
+                    
+                    x=eval(expression)
+                    while True:
+                        try:
+                            comment.reply(x)
+                            comments_responded.append(comment.id)
+                            with open(fil, "a") as f:
+                                f.write(comment.id + "\n")
+                            break
+                        except praw.exceptions.APIException:
+                            print("Waiting")
+                            time.sleep(60)
+                    
+
+            
+                
+            else:
+                ares_reply = random.choice(ares_quotes)
+                while True:
+                    try:
+                        comment.reply(ares_reply)
+                        comments_responded.append(comment.id)
+                        with open(fil, "a") as f:
+                            f.write(comment.id + "\n")
+                        break
+                    except praw.exceptions.APIException:
+                        print("Waiting")
+                        time.sleep(60)
 
