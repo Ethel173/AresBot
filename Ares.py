4759b3d1fd451585fc11879bc353cc94238ccb9d
Stefan <stefan_mack@hotmail.com>
2018-05-25 14:32:49 +0200

    made the bot into a class

--- Ares.py
+++ Ares.py
@@ -21,39 +21,42 @@ import re
 import os
 import time
 import sys
-reddit = praw.Reddit('Ares')
-subreddit = reddit.subreddit("pythonforengineers")
+class Bot():
+    def __init__(self, sub):
 
-fil = "responded.txt"    
-if not os.path.isfile(fil):
-    comments_responded = []
-    with open(fil, "w") as f:
-        pass
-else:
-    with open(fil, "r") as f:
-        comments_responded = f.read()
-        comments_responded = comments_responded.split("\n")
-        comments_responded = list(filter(None, comments_responded))
+        reddit = praw.Reddit('Ares')
+        self.subreddit = reddit.subreddit(sub)
+        fil = "responded.txt"    
+        if not os.path.isfile(fil):
+            self.comments_responded = []
+            with open(fil, "w") as f:
+                pass
+        else:
+            with open(fil, "r") as f:
+                self.comments_responded = f.read()
+                self.comments_responded = self.comments_responded.split("\n")
+                self.comments_responded = list(filter(None, self.comments_responded))
+    def begin(self):
 
-for comment in subreddit.stream.comments():
-    if comment.id not in comments_responded:
-        if re.search("Ares", comment.body, re.IGNORECASE):
-            if not comment.body.find('/') == -1:
-                start = comment.body.find('/')
-                if not comment.body.find('\\') == -1:
-                    stop = comment.body.find('\\')
-                    expression = comment.body[(start+1):stop]
-                    expression = expression.replace("^", "**")
-                    x=eval(expression)
-                    while True:
-                        try:
-                            comment.reply(x)
-                            comments_responded.append(comment.id)
-                            with open(fil, "a") as f:
-                                f.write(comment.id + "\n")
-                            break
-                        except praw.exceptions.APIException:
-                            time.sleep(60)
+        for comment in self.subreddit.stream.comments():
+            if comment.id not in self.comments_responded:
+                if re.search("Ares", comment.body, re.IGNORECASE):
+                    if not comment.body.find('/') == -1:
+                        start = comment.body.find('/')
+                        if not comment.body.find('\\') == -1:
+                            stop = comment.body.find('\\')
+                            expression = comment.body[(start+1):stop]
+                            expression = expression.replace("^", "**")
+                            x=eval(expression)
+                            while True:
+                                try:
+                                    comment.reply(x)
+                                    comments_responded.append(comment.id)
+                                    with open(fil, "a") as f:
+                                        f.write(comment.id + "\n")
+                                    break
+                                except praw.exceptions.APIException:
+                                    time.sleep(60)
                     
 
             
